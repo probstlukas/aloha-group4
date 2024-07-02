43dfdf850e91d9f4acb1239ab2f667ab11cfd34e
Jonathan Fricke <velolp05@gmail.com>
2024-07-02 12:02:22 +0200

    Prevent launchrobots script to start multiple times.

--- aloha_scripts/teleop_sim.sh
+++ aloha_scripts/teleop_sim.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-gnome-terminal -x ./launch_robots.sh
+if ! pgrep -f "launch_robots.sh" > /dev/null; then
+    gnome-terminal -x ./launch_robots.sh
+else
+    echo "launch_robots.sh is already running."
+fi
 
 source activate aloha
 python teleop_sim.py
