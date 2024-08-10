7596968c0847621f43d0074241435a3b28c53607
Group 4 <thelukasprobst@gmail.com>
2024-08-10 13:18:50 +0200

    Adjust teleop_sim script to execute new consitent script-name

--- aloha_scripts/teleop_sim_master.sh
+++ aloha_scripts/teleop_sim_master.sh
@@ -5,4 +5,4 @@ RELATIVE_PATH="../../aloha_platform_simulation/examples"
 
 gnome-terminal -x bash -c "./teleop_sim.sh; exec bash"
 
-sleep 10s && gnome-terminal --working-directory="${SCRIPT_DIR}/${RELATIVE_PATH}" -x bash -c "./teleop.sh; exec bash"
+sleep 10s && gnome-terminal --working-directory="${SCRIPT_DIR}/${RELATIVE_PATH}" -x bash -c "./teleop_sim.sh; exec bash"
